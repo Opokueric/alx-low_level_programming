a88d6f8c7540c79da6268ae02bf6bf3d66e4af49
Opokueric <opokueric790@gmail.com>
2023-06-15 14:49:58 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Header
+ *
+ * Description: Size is not grandeur, and territory does not make a nation
+ *
+ * Return: If 0, (Passed/Success)
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+		printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+		printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+			return (0);
+}
