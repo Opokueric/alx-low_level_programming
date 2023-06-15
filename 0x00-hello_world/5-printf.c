d9cff5817d09526ed4b7e96b199f13bd1159ad68
Opokueric <opokueric790@gmail.com>
2023-06-15 14:19:00 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- Main header
+ *
+ * Description: Print Hello
+ *
+ * Return: If 0 = Success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
