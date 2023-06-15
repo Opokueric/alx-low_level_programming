6defe05551cebf5695cba827c5d7106cd79dd476
Opokueric <opokueric790@gmail.com>
2023-06-15 12:54:41 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Description: This is a test program
+ *
+ * Return: If 0 (Passed)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
