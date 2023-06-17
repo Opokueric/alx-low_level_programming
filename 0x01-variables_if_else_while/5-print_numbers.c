25944352f3081d1c68a567890c024a4789075bac
Opokueric <opokueric790@gmail.com>
2023-06-16 18:26:25 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Header Entry point
+ *
+ * Return: If 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+		while (x < 10)
+	{
+		printf("%d", x;
+		++x;
+	}
+			printf("\n");
+
+	return (0);
+}
