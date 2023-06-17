e96f72310c7e1d688764ede6f0d8d71ba17cc1c1
Opokueric <opokueric790@gmail.com>
2023-06-16 17:53:54 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: If 0 (Pass)
+ */
+
+int main(void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		if (l != 'q' && c != 'e')
+			putchar(l);
+		++l;
+	}
+	putchar('\n');
+		return (0);
+}
