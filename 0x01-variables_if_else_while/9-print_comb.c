7f51023b497d7ae86f6255fd8210cfe2ac58788a
Opokueric <opokueric790@gmail.com>
2023-06-16 19:38:25 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Header
+ *
+ * Return: If 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 48;
+
+	while (x < 58)
+	{
+		putchar(d);
+		if (x != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		++x;
+	}
+	putchar('\n');
+
+	return (0);
+}
