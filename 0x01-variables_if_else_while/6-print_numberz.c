b0fd6f65ca018d65ac0354c57db518a38f2070a3
Opokueric <opokueric790@gmail.com>
2023-06-16 18:44:37 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Header
+ *
+ * Return: If  0 (Success)
+ */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+		putchar(n);
+		++n;
+	}
+		putchar('\n');
+
+	return (0);
+}
