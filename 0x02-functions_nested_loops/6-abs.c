a416fd6917e888e2f42406aa16b1ac3adb0086f8
Opokueric <opokueric790@gmail.com>
2023-06-20 14:16:06 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs- computes the absolute value of an integer
+ * @x: x absolute
+ *
+ * Return: x
+ */
+
+int _abs(int x)
+{
+	if (x < 0)
+	{
+	return (x * -1);
+	}
+	else
+	{
+	return (x);
+	}
+}
