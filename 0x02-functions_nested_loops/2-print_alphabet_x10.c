45a4f7f75de5e4fa3eea3592db125fd4160686f8
Opokueric <opokueric790@gmail.com>
2023-06-20 12:02:27 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  prints the 10x the lowercase alphabet
+ *
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char al = 'a';
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 26; y++)
+		{
+			_putchar(al + y);
+		}
+		_putchar(10);
+	}
+}
