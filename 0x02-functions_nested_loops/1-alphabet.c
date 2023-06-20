36455f998aa4d2f61e6cab29edc3a05402a9480e
Opokueric <opokueric790@gmail.com>
2023-06-20 11:17:15 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints the lowercase alphabet
+ *
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; ++x)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
