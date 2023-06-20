170444f2238e81918b639f1a62aede04f8ef9060
Opokueric <opokueric790@gmail.com>
2023-06-20 09:00:23 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- Print _putchar
+ *
+ * Description: a program that prints _putchar, followed by a new line
+ *
+ * Return: 0 if Success
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
