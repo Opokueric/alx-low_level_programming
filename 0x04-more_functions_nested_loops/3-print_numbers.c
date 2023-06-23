6a962076328ce99ae44e6fbe58bfdc8863384ad6
Opokueric <opokueric790@gmail.com>
2023-06-22 19:03:03 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9 without using standard functions
+ *
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int x = 48;
+
+	while (x < 58)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
