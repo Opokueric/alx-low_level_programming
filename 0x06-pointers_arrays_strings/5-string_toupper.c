5272f4f54b33dc8ed416a76d1144897ba360388f
Opokueric <opokueric790@gmail.com>
2023-06-29 18:32:37 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - function to transform all lowercase in string to uppercase
+ *
+ * @p: string
+ * Return: char
+ */
+
+char *string_toupper(char *p)
+{
+	int x = 0;
+
+	while (p[i] != '\0')
+	{
+		if (p[x] > 96 && p[x] < 123)
+			p[x] -= 32;
+		x++;
+	}
+	return (p);
+}
