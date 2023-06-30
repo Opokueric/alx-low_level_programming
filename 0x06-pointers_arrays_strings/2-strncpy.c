4273c0b5af443f31a06bb95e22e546ceeae87d0b
Opokueric <opokueric790@gmail.com>
2023-06-29 18:24:03 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - function to copy string
+ *
+ * @dest: string to copy to
+ * @src: string to copy from
+ * @n: number of chars
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x, y;
+
+	for (x = 0; x < n; x++)
+	{
+		if (src[x] == '\0')
+		{
+			for (y = x; j < n && dest[y] != '\0'; y++)
+				dest[y] = '\0';
+			break;
+		}
+		dest[x] = src[x];
+	}
+
+	return (dest);
+}
